3ed57a6c505d6c612b349983f5367d37dbea1165
siafromspace <sharonyak15@gmail.com>
2022-06-24 21:06:04 +0100

    menu bar

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const mobileMenu = document.querySelector(".mobile-menu")
-const hamburger = document.querySelector(".mobile-menu")
+const menuBtn = document.getElementById("menu-btn")
+const closeMenu = document.querySelector(".close-menu")
 
-hamburger.addEventListener("click", () => {
-    mobileMenu.style.visibility = "visible"
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.add('active')
+})
+closeMenu.addEventListener("click", () => {
+    mobileMenu.classList.remove('active')
 })
