c63a961980c221ebc9c0b1365a040bacfcf634cf
siafromspace <sharonyak15@gmail.com>
2022-06-24 21:36:21 +0100

    edits

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const mobileMenu = document.querySelector(".mobile-menu")
 const menuBtn = document.getElementById("menu-btn")
 const closeMenu = document.querySelector(".close-menu")
+const overlay = document.querySelector(".overlay")
+
 
 menuBtn.addEventListener("click", () => {
     mobileMenu.classList.add('active')
+    overlay.classList.add('enabled')
 })
 closeMenu.addEventListener("click", () => {
     mobileMenu.classList.remove('active')
+    overlay.classList.remove('enabled')
 })
