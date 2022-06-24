1c02ac18d484a6513b8046fcb67f87d61de26e9b
siafromspace <sharonyak15@gmail.com>
2022-06-24 20:46:40 +0100

    mobile menu

---
+++ script.js
@@ -0,0 +1,6 @@
+const mobileMenu = document.querySelector(".mobile-menu")
+const hamburger = document.querySelector(".mobile-menu")
+
+hamburger.addEventListener("click", () => {
+    mobileMenu.style.visibility = "visible"
+})
